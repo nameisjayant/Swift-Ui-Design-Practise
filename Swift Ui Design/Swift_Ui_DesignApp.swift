5e95c223ab3e329efe5a6ea1407342438e4c2b65
nameisjayant <jayntkumar99@gmail.com>
2022-09-09 22:08:37 +0530

    Initial Commit

---
+++ Swift Ui Design/Swift_Ui_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Ui_DesignApp.swift
+//  Swift Ui Design
+//
+//  Created by jayant kumar on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Ui_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
